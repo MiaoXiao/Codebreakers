06e7550a80ea02a67ef57530ecfa54acadecc7da
MiaoXiao <topgb@msn.com>
2015-03-04 23:48:14 -0800

    rudimentary gameplay possible

--- Codebreakers/Codebreakers.c
+++ Codebreakers/Codebreakers.c
@@ -23,15 +23,15 @@
 //value of last input
 unsigned char lastInput;
 
-
 //--------Helper Functions---------------------------------
 //returns a series of characters, up to a certain length
 //custom is a bool to determine whether to use custom characters or not
-char* getRandKey(int length, int custom)
+unsigned int length = 5;
+static char answerKey[5];
+void getRandKey(int custom)
 {
 	char allKeypad[15] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', '\0'};
 	//randomly generated key that both players must copy
-	char key[length];
 	int i = 0;
 	if (custom)
 	{
@@ -39,89 +39,136 @@ char* getRandKey(int length, int custom)
 	}
 	else
 	{
-		for (i = 0; key[i] != '\0'; ++i)
+		for (i = 0; i < length; ++i)
 		{
-			key[i] = allKeypad[rand() % 15];
+			answerKey[i] = allKeypad[rand() % 15];
+		}
+		answerKey[i] = '\0';
+	}
+}
+
+//checks to see if a character is the correct one in the sequence
+unsigned int anspos = 0;
+unsigned char lights = 0x00;
+const unsigned char win[] = {'Y', 'o', 'u', ' ', 'W', 'i', 'n', '!', '\0'};
+void checkKey(char character)
+{
+	if (answerKey[anspos] == character) //correct
+	{
+		anspos++;
+		switch (anspos)
+		{
+			case 1:
+				lights = 0x01;
+				break;
+			case 2:
+				lights = 0x03;
+				break;
+			case 3:
+				lights = 0x07;
+				break;
+			case 4:
+				lights = 0x0F;
+				break;
+			case 5:
+				lights = 0x1F;
+				LCD_DisplayString(1, win);
+				anspos = 0;
 		}
 	}
-	return key;
+	else //incorrect
+	{
+		anspos = 0;
+		lights = 0x00;
+	}
+	PORTB = lights;
+	
 }
 
 //--------User defined FSMs--------------------------------
 unsigned char Flag;
-enum keyStates{keyStart, keyWait, keyOut} keystate;
-int keyPad(int state)
+
+enum gameStates{gameStart, gameInit, gameWait, gameOut} gamestate;
+int gameManager(int state)
 {
 	unsigned char x;
 	
-	switch(keystate)
+	switch(gamestate)
 	{
-		case keyStart:
-		keystate = keyOut;
+		case gameStart:
+			gamestate = gameInit;
+			break;
+		case gameInit:
+			gamestate = gameOut;
+			break;
+		case gameWait:
+			x = GetKeypadKey();
+			if(x == '\0')
+			{
+				gamestate = gameWait;
+				Flag = 0;
+			}
+			else
+			{
+				gamestate = gameOut;
+			}
 		break;
-		case keyWait:
-		x = GetKeypadKey();
-		if(x == '\0')
-		{
-			keystate = keyWait;
-			Flag = 0;
-		}
-		else
-		{
-			keystate = keyOut;
-		}
-		break;
-		case keyOut:
-		keystate = keyWait;
+			case gameOut:
+			gamestate = gameWait;
 		break;
 	}
 	
 	if(!Flag)
 	{
-		switch(keystate)
+		switch(gamestate)
 		{
-			case keyStart:
+			case gameStart:
 				break;
-			case keyWait:
+			case gameInit:
+				//generate random key
+				getRandKey(0);
 				break;
-			case keyOut:
-			LCD_Cursor(1);
-			x = GetKeypadKey();
-			switch (x) {
-				// All 5 LEDs on
-				case '\0': PORTB = 0x1F; break;
-				// hex equivalent
-				case '1': LCD_WriteData('1'); lastInput = '1';
+			case gameWait:
 				break;
-				case '2': LCD_WriteData('2'); lastInput = '2';
-				break;
-				case '3': LCD_WriteData('3'); lastInput = '3';
-				break;
-				case '4': LCD_WriteData('4'); lastInput = '4';
-				break;
-				case '5': LCD_WriteData('5'); lastInput = '5';
-				break;
-				case '6': LCD_WriteData('6'); lastInput = '6';
-				break;
-				case '7': LCD_WriteData('7'); lastInput = '7';
-				break;
-				case '8': LCD_WriteData('8'); lastInput = '8';
-				break;
-				case '9': LCD_WriteData('9'); lastInput = '9';
-				break;
-				case 'A': LCD_WriteData('A'); lastInput = 'A';
-				break;
-				case 'B': LCD_WriteData('B'); lastInput = 'B';
-				break;
-				case 'C': LCD_WriteData('C'); lastInput = 'C';
-				break;
-				case 'D': LCD_WriteData('D'); lastInput = 'D';
-				break;
-				case '*': LCD_WriteData('*'); lastInput = '*';
-				break;
-				case '0': LCD_WriteData('0'); lastInput = '0';
-				break;
-				case '#': LCD_WriteData('#'); lastInput = '#';
+			case gameOut:
+				LCD_ClearScreen();
+				LCD_Cursor(1);
+				x = GetKeypadKey();
+				switch (x) {
+					// All 5 LEDs on
+					case '\0': PORTB = 0x1F; break;
+					// hex equivalent
+					case '1': LCD_WriteData('1'); checkKey('1');
+					break;
+					case '2': LCD_WriteData('2'); checkKey('2');
+					break;
+					case '3': LCD_WriteData('3'); checkKey('3');
+					break;
+					case '4': LCD_WriteData('4'); checkKey('4');
+					break;
+					case '5': LCD_WriteData('5'); checkKey('5');
+					break;
+					case '6': LCD_WriteData('6'); checkKey('6');
+					break;
+					case '7': LCD_WriteData('7'); checkKey('7');
+					break;
+					case '8': LCD_WriteData('8'); checkKey('8');
+					break;
+					case '9': LCD_WriteData('9'); checkKey('9');
+					break;
+					case 'A': LCD_WriteData('A'); checkKey('A');
+					break;
+					case 'B': LCD_WriteData('B'); checkKey('B');
+					break;
+					case 'C': LCD_WriteData('C'); checkKey('C');
+					break;
+					case 'D': LCD_WriteData('D'); checkKey('D');
+					break;
+					case '*': LCD_WriteData('*'); checkKey('*');
+					break;
+					case '0': LCD_WriteData('0'); checkKey('0');
+					break;
+					case '#': LCD_WriteData('#'); checkKey('#');
 				break;
 				// Should never occur. Middle LED off.
 				default: PORTB = 0x1B; break;
@@ -131,9 +178,8 @@ int keyPad(int state)
 		}
 	}
 	
-	return keystate;
+	return gamestate;
 }
-
 
 enum menusStates{menuStart, startscreen, gameconfigscreen, playscreen, back} menustate;
 int menus(int state)
@@ -143,10 +189,8 @@ int menus(int state)
 
 // Implement scheduler code from PES.
 int main()
-{
-	//random seed
-	//srand(time(NULL));
-	
+{	
+	srand(100);
 	// Set Data Direction Registers
 	// Buttons PORTA[0-7], set AVR PORTA
 	// to pull down logic
@@ -164,22 +208,22 @@ int main()
 	unsigned long int menus_period = 1;
 	
 	//Declare an array of tasks
-	static task task1, task2, task3, task4;
+	static task task1, task2;
 	task *tasks[] = {&task1, &task2};
 	const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
 	
 	// Task 1
-	task1.state = keyStart;
+	task1.state = gameStart;
 	task1.period = keyPad_period;
 	task1.elapsedTime = keyPad_period;
-	task1.TickFct = &keyPad;
+	task1.TickFct = &gameManager;
 	
 	//Task 2
 	task2.state = menuStart;
 	task2.period = menus_period;
 	task2.elapsedTime = menus_period;
 	task2.TickFct = &menus;
-	//Task 3
+	
 	
 	// Set the timer and turn it on
 	TimerSet(1);
